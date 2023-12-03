d2e2e15783fc892f73391d3f2512cbef9374745b
AteshanGunduz <145977486+AteshanGunduz@users.noreply.github.com>
2023-12-03 12:58:09 +0300

    Initial commit

---
+++ src/ImageList.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function ImageList({ images }) {
+  const [selectedName, setSelectedName] = useState("");
+
+  const handleClick = (name) => {
+    setSelectedName(name);
+  };
+
+  const renderedImages = images.map((image) => (
+    <img
+      key={image.id}
+      src={image.src}
+      onClick={() => handleClick(image.name)}
+    />
+  ));
+
+  return (
+    <div>
+      <h1>Image ismi: {selectedName}</h1>
+      <div>{renderedImages}</div>
+    </div>
+  );
+}
+
+export { ImageList };
